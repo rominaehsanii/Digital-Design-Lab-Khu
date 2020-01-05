4d0b3119261653c1cfb224986a085ebbb0ccdc52
rominaehsanii <rominaehsanii@gmail.com>
2020-01-06 01:25:08 +0330

    ALU and ALU Control modules implemented

---
+++ CPU/alu.v
@@ -0,0 +1,21 @@
+module ALU #(parameter n = 64,parameter delay = 100)(input_a, input_b, opcode, output_z, output_data);
+	input [n-1:0] input_a;
+	input [n-1:0] input_b;
+	input [3:0] opcode;
+	output reg [63:0]output_data;
+	output output_z;
+
+assign output_z = output_data ? 0 : 1 ;
+always @(*)begin
+	output_data = 64'hz;
+	case(opcode)
+		4'b0000: output_data = input_a & input_b;
+		4'b0001: output_data = input_a | input_b;
+		4'b0010: output_data = input_a + input_b;
+		4'b0110: output_data = input_a - input_b;
+
+	endcase
+end
+
+
+endmodule
---
+++ CPU/alucontrol.v
@@ -0,0 +1,18 @@
+module Alu_Control#(parameter delay = 100)(input [1:0] ALUOp,input [31:25] Funct7,input [14:12] Funct3,output reg [3:0] operation);
+    
+    assign key_bits = {ALUOp, Funct7[30], Funct3};
+    
+    always @(key_bits) begin
+      operation = 4'bz;
+
+      casex(key_bits)
+        6'b00xxxx: operation = 4'b0010;
+        6'bx1xxxx: operation = 4'b0110;
+        6'b1x0000: operation = 4'b0010;
+        6'b1x1000: operation = 4'b0110;
+        6'b1x0111: operation = 4'b0000;
+        6'b1x0110: operation = 4'b0001;
+      endcase
+    end
+  
+endmodule
